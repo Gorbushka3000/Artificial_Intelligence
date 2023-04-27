d5d4437154edd514e544b4c7c274d08872252bf3
gorbushka <glebdeshar@gmail.com>
2023-04-27 15:41:19 +0300

    Обучение без учителя с графиками

---
+++ ElementsAI/learningWithoutTeacher.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.colors import ListedColormap
+
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+df = pd.read_csv(url, header=None)
+print('Данные об ирисах')
+print(df.to_string())
+
+y = df.iloc[0:100, 4].values
+print(y)
+
+y = np.where(y == "Iris-setosa", -1, 1)
+print(y)
+
+X = df.iloc[0:100, [0, 2]].values
+print(X)
+print('end x')
+
+plt.scatter(X[0:50, 0], X[0:50, 1], color='red', marker='o', label='щетинситый')
+plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='разноцветный')
+plt.xlabel('Длина чашелистника')
+plt.ylabel('Длина лепестка')
+plt.legend(loc='upper left')
+plt.show()
+
+
+class Perceptron(object):
+
+    def __init__(self, eta=0.01, n_iter=10):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, x, y):
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.errors_ = []
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+
+ppn = Perceptron(0.1, 10)
+ppn.fit(X, y)
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+plt.xlabel('Эпохи')
+plt.ylabel('Случаи ошибочной классификации')
+plt.show()
+
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'green', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl)
+
+
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel('длина чашелистника')
+plt.ylabel('длина лепестка')
+plt.legend(loc='upper left')
+plt.show()
